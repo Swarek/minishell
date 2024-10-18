caedb950dd47e2c90b0690d65e76e38d6bbe4140
Mathis Blanc <mblanc@paul-f4Ar8s8.clusters.42paris.fr>
2024-10-18 20:37:28 +0200

    Merged Libft mathis

--- Libft/src/error_handling/error_msg.c
+++ Libft/src/error_handling/error_msg.c
@@ -1,39 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   error_msg.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mblanc <mblanc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/21 00:08:39 by mblanc            #+#    #+#             */
-/*   Updated: 2024/10/18 09:32:26 by mblanc           ###   ########.fr       */
+/*   Created: 2024/09/04 12:32:54 by mblanc            #+#    #+#             */
+/*   Updated: 2024/10/18 10:01:58 by mblanc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+int	error_msg(const char *msg)
 {
-	int		sign;
-	long	result;
-	int		i;
-
-	sign = 1;
-	result = 0;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (msg)
 	{
-		if (str[i] == '-')
-			sign = -1;
-		i++;
+		write(2, BOLD_COLOR_RED, 8);
+		write(2, "Error\n", 6);
+		while (*msg)
+		{
+			write(2, msg, 1);
+			msg++;
+		}
+		write(2, RESET_COLOR, 4);
 	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (sign * result);
+	return (-1);
 }
